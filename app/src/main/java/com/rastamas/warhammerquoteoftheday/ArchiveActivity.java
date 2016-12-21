2965b548ce5914a0c653b3066804d7d293fe3e05
Rastamas <r.tamas.szabo@gmail.com>
2016-12-21 01:29:38 +0100

    Add timestamps to archive.

--- app/src/main/java/com/rastamas/warhammerquoteoftheday/ArchiveActivity.java
+++ app/src/main/java/com/rastamas/warhammerquoteoftheday/ArchiveActivity.java
@@ -8,8 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ArchiveActivity extends AppCompatActivity {
 
@@ -23,17 +33,38 @@ public class ArchiveActivity extends AppCompatActivity {
         Button clearArchivesButton = (Button) findViewById(R.id.button_clear_archives);
         clearArchivesButton.setTypeface(custom_font);
 
-        Map archives = MainActivity.mArchives.getAll();
-        for (Object record :
-                archives.values()) {
+        TreeMap archives = new TreeMap(MainActivity.mArchives.getAll());
+        for (Object entry :
+                archives.descendingKeySet()) {
+            String value = archives.get(entry.toString()).toString();
             TextView recordTextView = new TextView(getApplicationContext());
-            recordTextView.setText(record.toString());
+            recordTextView.setText(value);
             recordTextView.setTextColor(getResources().getColor(R.color.emperorAccent));
             recordTextView.setTypeface(custom_font);
-            recordTextView.setTextSize(24);
+            recordTextView.setTextSize(28);
 
+            TextView dateTextView = new TextView(getApplicationContext());
+            dateTextView.setText(prettifyDate(entry.toString()));
+            dateTextView.setTextColor(getResources().getColor(R.color.emperorPrimary));
+            dateTextView.setTypeface(custom_font);
+            dateTextView.setTextSize(18);
+
+            mArchiveLayout.addView(dateTextView);
             mArchiveLayout.addView(recordTextView);
         }
+    }
+
+    private String prettifyDate(String dateToParse){
+        Date date;
+        try {
+            date = new SimpleDateFormat("yyyyMMMdd").parse(dateToParse);
+        } catch (ParseException e) {
+            return dateToParse;
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        return cal.get(Calendar.YEAR) + " - " + cal.get(Calendar.MONTH) + " - " + cal.get(Calendar.DAY_OF_MONTH);
     }
 
     public void clearArchives(View view) {
